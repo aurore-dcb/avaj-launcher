c38bfdd2a4e72e976f714535fe941f138e6f1075
aducobu <aurore.ducobu@planisense.com>
2025-01-02 11:32:16 +0100

    parse error ok

--- srcs/AvajLauncher.java
+++ srcs/AvajLauncher.java
@@ -21,65 +21,60 @@ class AvajLauncher {
 
     public static boolean verifyInputLineContent(String line) {
         String[] splitLine = line.split(" ");
-        // System.out.printf("length: %d\n", splitLine.length);
         if (splitLine.length != 5)
             return true;
-        // 1er elem : String
         if (splitLine[0].matches("^[a-zA-Z]*$") == false) {
-            // System.out.println("false");
             return true;
         }
-        // 2e  elem : Char + Int
-        // 3e et 4e elem : int
+         if (splitLine[1].matches("^[A-Z0-9]*$") == false) {
+            return true;
+        }
         try {
             Integer.parseInt(splitLine[2]);
             Integer.parseInt(splitLine[3]);
             int height = Integer.parseInt(splitLine[4]);
-            if (height <= 0 || height >= 100)
+            if (height <= 0 || height > 100)
                 return true;
         } catch (Exception e) {
             return true;
         }
-        // 5e elem : 0 < int < 100
-        // for (String s : splitLine) {
-        //     System.out.println(s);
-        // }
+        // stocker ?
         return false;
     }
 
-    public static void Parser (String file) {
+    public static void Parser (String file) throws Exception {
         int nb_line = 0;
-        try {
-            FileReader fileReader = new FileReader(file);
-            BufferedReader reader = new BufferedReader(fileReader);
-            String line = reader.readLine();
+        FileReader fileReader = new FileReader(file);
+        BufferedReader reader = new BufferedReader(fileReader);
+        String line = reader.readLine();
 
-            while (line != null && !line.isEmpty()) {
-                if (nb_line == 0) {
-                    if (verifyInputFirstLine(line) == true) {
-                        throw new Exception("parse error: wrong format line " + nb_line + ".");
-                    }
-                } else {
-                    if (verifyInputLineContent(line) == true) {
-                        throw new Exception("parse error: wrong format line " + nb_line + ".");
-                    }
+        while (line != null && !line.isEmpty()) {
+            if (nb_line == 0) {
+                if (verifyInputFirstLine(line) == true) {
+                    throw new Exception("wrong format line " + (nb_line + 1) + ".");
+                 }
+            } else {
+               if (verifyInputLineContent(line) == true) {
+                   throw new Exception("wrong format line " + (nb_line + 1) + ".");
                 }
-                // System.out.println(line);
-                nb_line++;
-                line = reader.readLine();
             }
-
-            if (nb_line < 2) {
-                throw new Exception("parse error: wrong format input file.");
-            }
-            reader.close();
-        } catch (Exception e) {
-            System.err.println(e);
+            nb_line++;
+            line = reader.readLine();
         }
+
+        if (nb_line < 2) {
+            throw new Exception("wrong format input file.");
+        }
+        reader.close();
     }
-    public static void main (String[] args){
-        Parser(args[0]);
-        System.out.println("Parsing OK");
+    public static void main (String[] args) {
+        try {
+            Parser(args[0]);
+            System.out.println("Parsing OK");
+        } catch (Exception e) {
+            System.err.println("parse error: " + e.getMessage()); 
+            System.exit(1);
+        }
         // Coordinates coordinates1 = new Coordinates(654,33,20);
         // AircraftFactory factory = AircraftFactory.getInstance();
         // Flyable heli = factory.newAircraft("Helicopter", "H1", coordinates1);
